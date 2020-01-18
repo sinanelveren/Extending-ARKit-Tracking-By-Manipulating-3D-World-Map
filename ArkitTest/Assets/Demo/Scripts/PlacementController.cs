e8aaef26f04291ae74a6dd102ccd864351de4778
Sinan Elveren <sinanelveren@gmail.com>
2020-01-18 18:35:11 +0300

    Model Target & ARKit Test Apps

---
+++ ArkitTest/Assets/Demo/Scripts/PlacementController.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using TMPro.EditorUtilities;
+using UnityEngine;
+//using UnityEngine.TestTools.Utils;
+//using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+using UnityEngine.XR.ARSubsystems;
+
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(ARRaycastManager))]
+/**
+ * @author Sinan Elveren
+ * Controller for reference point of carcass
+ */
+public class PlacementController : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    public GameObject PlacedPrefab
+    {
+        get 
+        {
+            return placedPrefab;
+        }
+        set 
+        {
+            placedPrefab = value;
+        }
+    }
+
+    public ARRaycastManager arRaycastManager;
+    
+    [SerializeField]
+    private GameObject placedFrame;
+    
+    //*
+    
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    private Vector3 cameraForward;
+    private Vector3 cameraBearing;
+   
+    public GameObject placementIndicator;
+    private GameObject robot;
+    private GameObject frame;
+    private Vector3 initialScale;
+    private Vector3 initialScaleF;
+    private int scaleCount = 1;
+    private Pose initialPose;
+
+    private int objectCount = 0;
+    //*
+    static string myLog = "";
+    private string output;
+    private string stack;
+    Queue myLogQueue = new Queue();
+
+    
+    
+    void Awake() 
+    {
+        //arRaycastManager = GetComponent<ARRaycastManager>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        
+        
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.FeaturePoint);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            cameraForward = Camera.current.transform.forward;
+            cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+    
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            //if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            objectCount++;
+            
+            if (objectCount == 1)
+            {
+                robot = Instantiate(placedPrefab, placementPose.position, placementPose.rotation);
+                frame = Instantiate(placedFrame, placementPose.position, placementPose.rotation);
+                robot.transform.localScale /= 10;
+                frame.transform.localScale /= 10;
+                initialScale = robot.transform.localScale;
+                initialScaleF = frame.transform.localScale;
+                initialPose = placementPose;
+
+                //  Debug.Log("pose " + placementPose.position);
+            }
+            
+            //robot.transform.localScale += new Vector3(10.0f, 10.0f, 10.0f);
+            //robot.transform.position += new Vector3(100.0f, 100.0f, 100.0f);
+        }
+
+       
+
+    }
+    
+    
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        myLog = logString;
+        string newString = "\n [" + type + "] : " + myLog;
+        myLogQueue.Enqueue(newString);
+        if (type == LogType.Exception)
+        {
+            newString = "\n" + stackTrace;
+            myLogQueue.Enqueue(newString);
+        }
+        myLog = string.Empty;
+        foreach(string mylog in myLogQueue){
+            myLog += mylog;
+        }
+    }
+        
+        
+    void OnGUI()
+    {
+        
+        if (GUI.Button(new Rect(Screen.width - 300, Screen.height - 200 - 250, 250, 100), "BÜYÜT"))
+        {
+            if (scaleCount < 10)
+            {
+                ++scaleCount;
+                robot.transform.localScale =  initialScale * scaleCount;
+                frame.transform.localScale =  initialScaleF * scaleCount;
+                
+                //The SceneManager loads your new Scene as a single Scene (not overlapping). This is Single mode.
+                //SceneManager.LoadScene("front", LoadSceneMode.Single);
+            }
+        }
+
+    
+        if (GUI.Button(new Rect(Screen.width - 300, Screen.height - 200 - 100, 250, 100), "KÜÇÜLT"))
+        {
+            if (scaleCount > 1)
+            {
+                --scaleCount;
+                robot.transform.localScale = initialScale * scaleCount;
+                frame.transform.localScale = initialScaleF * scaleCount;
+                //The SceneManager loads your new Scene as a single Scene (not overlapping). This is Single mode.
+                //SceneManager.LoadScene("front", LoadSceneMode.Single);
+            }
+        }
+        
+
+        //if (!Application.isEditor) //Do not display in editor ( or you can use the UNITY_EDITOR macro to also disable the rest)
+       /* {
+             GUI.Label(new Rect(10, 10, Screen.width - 10, Screen.height - 10), myLog);
+        }*/
+    }
+
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+}
---
+++ model-target-lenovo/Assets/RotateRight.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Xsl;
+using UnityEngine;
+
+public class RotateRight : MonoBehaviour
+{
+
+	private Vector3 pos;
+	private int count;
+	private int way;
+	
+	float maxValue = 0.05f; // or whatever you want the max value to be
+	float currentValue = 0f; // or wherever you want to start
+	int direction = 1;
+	private bool flag = true;
+
+
+	
+	void Update()
+	{
+		if (flag)
+		{
+			pos = transform.localPosition;
+			flag = false;
+		}
+		currentValue = Time.deltaTime*0.25f; // or however you are incrementing the position
+		if(System.Math.Abs(transform.localPosition.x - pos.x) > 0.3f) {
+			 direction *= -2;
+			//currentValue = 0;
+			//transform.localPosition = pos;
+		}
+
+		if (transform.localPosition.x < pos.x)
+		{
+			direction = 1;
+		}
+		
+		
+		
+		transform.Rotate(new Vector3(0f, 50f, 0f) * Time.deltaTime);
+		//transform.Translate(new Vector3(0f,  currentValue, 0f));
+		transform.localPosition += new Vector3(currentValue*direction,0f, 0f);
+
+		/*	var transformPosition = transform.position;
+			transformPosition.x +=50f * Time.deltaTime;
+
+			transform.position = transformPosition;
+		*/
+
+
+
+	}
+}
